afd7c6103ca6adcbb16ddd680805ac8846b5e3be
Lucas Pinheiro <115811060+lucaspinheiro27@users.noreply.github.com>
2024-11-20 22:54:00 -0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,26 @@
+az group create --name GSResource --location brazilsouth
+
+az storage account create --name globalsolutionstorage --resource-group GSResource --location brazilsouth --sku Standard_LRS
+
+az storage container create --account-name globalsolutionstorage --name gscointainer
+
+az cosmosdb create --name gscosmosdb --resource-group GSResource --kind MongoDB --locations regionName=brazilsouth failoverPriority=0 isZoneRedundant=False
+
+az cosmosdb mongodb database create \
+--account-name  gscosmosdb \
+--name mydatabase \
+--resource-group GSResource 
+
+az cosmosdb mongodb collection create --account-name gscosmosdb --database-name mydatabase --name mycollection1 --resource-group GSResource --throughput 400
+
+
+
+
+
+
+
+
+
+az cosmosdb mongodb collection create --account-name  gscosmosdb --database-name mydatabase --name mycollection --resource-group GSResource --shard 'key' --throughput 400
+
+az cosmosdb mongodb collection create --account-name gscosmosdb  --database-name mydatabase --name ml-collection --resource-group GSResource --shard 'key' --throughput 400
